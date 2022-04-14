a3c6f8581ef23d69a341439dd747df7ba4150cbf
AxelPerezdlg <axelperez859@gmail.com>
2022-04-14 00:42:35 -0500

    proyecto modificado

--- src/main/java/mx/edu/utez/proyecto/controller/HorarioController.java
+++ src/main/java/mx/edu/utez/proyecto/controller/HorarioController.java
@@ -3,6 +3,7 @@ package mx.edu.utez.proyecto.controller;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -29,24 +30,25 @@ import mx.edu.utez.proyecto.service.VentanillaService;
 @Controller
 @RequestMapping(value = "/horarios")
 public class HorarioController {
-    
-    @Autowired
-    private HorarioService horarioService;
-
-    @Autowired
-    private VentanillaService ventanillaService;
 
 	@Autowired
-    private UsuarioService usuarioService;
+	private HorarioService horarioService;
 
-    @GetMapping(value = "/listar" )
-	public String listarHorarios( Model modelo, Pageable pageable) {
-        Page<Horario> listaHorarios = horarioService.listarPaginacion(PageRequest.of(pageable.getPageNumber(), 5, Sort.by("dia").ascending()));
+	@Autowired
+	private VentanillaService ventanillaService;
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@GetMapping(value = "/listar")
+	public String listarHorarios(Model modelo, Pageable pageable) {
+		Page<Horario> listaHorarios = horarioService
+				.listarPaginacion(PageRequest.of(pageable.getPageNumber(), 5, Sort.by("dia").ascending()));
 		modelo.addAttribute("listaHorarios", listaHorarios);
 		return "listaHorarios";
 	}
 
-    @GetMapping("/crear")
+	@GetMapping("/crear")
 	public String crearHorario(Horario horario, Model modelo) {
 		modelo.addAttribute("listaVentanilla", ventanillaService.listar());
 		modelo.addAttribute("listaUsuarios", usuarioService.listar());
@@ -55,15 +57,13 @@ public class HorarioController {
 		return "formHorarios";
 	}
 
-    @GetMapping("/editar/{idhorarios}")
+	@GetMapping("/editar/{idhorarios}")
 	public String editar(@PathVariable long idhorarios, Model modelo, RedirectAttributes redirectAttributes) {
 		Horario horario = horarioService.mostrar(idhorarios);
 		if (horario != null) {
 			modelo.addAttribute("horario", horario);
-            SimpleDateFormat formatterDate = new SimpleDateFormat("yyyy-MM-dd");
-            modelo.addAttribute("fechaDiaUpdate", formatterDate.format(horario.getDia()));
-
-           
+			SimpleDateFormat formatterDate = new SimpleDateFormat("yyyy-MM-dd");
+			modelo.addAttribute("fechaDiaUpdate", formatterDate.format(horario.getDia()));
 
 			modelo.addAttribute("listaVentanilla", ventanillaService.listar());
 			modelo.addAttribute("listaUsuarios", usuarioService.listar());
@@ -73,94 +73,139 @@ public class HorarioController {
 		return "redirect:/horarios/listar";
 	}
 
-    @PostMapping("/guardar")
+	@PostMapping("/guardar")
 	public String guardarHorario(
-	@RequestParam("fechaDia") String fechaDia,
-    @RequestParam("hora") String hora,Horario horario, Model modelo, RedirectAttributes redirectAttributes) throws ParseException{
-        
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        Date fechaDiaRegistro =  formatter.parse(fechaDia);
+			@RequestParam("fechaDia") String fechaDia,
+			@RequestParam("hora") String hora, Horario horario, Model modelo, RedirectAttributes redirectAttributes)
+			throws ParseException {
 
-        String errorMessage = "";
-        boolean bandera = true;
-		String successMessage ="";	
-
-		if (horario.getIdhorarios() == null) { // Create
-            List<Horario> listaDia = horarioService.listarPorDiaAndVentanilla(fechaDiaRegistro, horario.getVentanilla().getIdventanilla());
-			for (int i = 0; i < listaDia.size(); i++) {
-              
-                if(hora.equals(listaDia.get(i).getHora()) ){
-                    errorMessage = "La ventanilla " + listaDia.get(i).getVentanilla().getNumero() + " ya no puede registrar la misma hora";
-                    bandera = false;
-                }
-            }
-            if(bandera){
-                horario.setDia(fechaDiaRegistro);
-			    horario.setHora(hora);
-                successMessage = "Horario Registrado Exitosamente";
-            }
-            
-		
-		} else { // Update
-            List<Horario> listaDia = horarioService.listarPorDiaAndVentanillaAndHorario(fechaDiaRegistro, horario.getVentanilla().getIdventanilla(), horario.getIdhorarios());
-			for (int i = 0; i < listaDia.size(); i++) {
-              
-                if(hora.equals(listaDia.get(i).getHora()) ){
-                    errorMessage = "La ventanilla " + listaDia.get(i).getVentanilla().getNumero() + " ya no puede registrar la misma hora";
-                    bandera = false;
-                }
-            }
-            if(bandera){
-                horario.setDia(fechaDiaRegistro);
-                horario.setHora(horario.getHora());
-			    horario.setRepeticiones(horario.getRepeticiones());
-			    horario.setUsuario(horario.getUsuario());
-                horario.setVentanilla(horario.getVentanilla());
-                successMessage = "Horario Modificado Exitosamente";
-            }
-            
-			
+		boolean isRegistro = true;
+		if (horario.getIdhorarios() != null) {
+			isRegistro = false;
 		}
 
+		int repeticiones = 1;
+		if (horario.getRepeticiones() != null) {
+			repeticiones = horario.getRepeticiones();
+		}
 
-    
-		
 		boolean respuesta = false;
+		String successMessage = "";
+		String errorMessage = "";
+		String fechaTempo = fechaDia;
+		int registrosExitosos = 0;
 
-		if(bandera){
-			respuesta = horarioService.guardar(horario);
-			if(!respuesta){
-				errorMessage =  "Registro Fallido por favor intente de nuevo";
+		for (int x = 0; x < repeticiones; x++) {
+
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+			Date fechaDiaRegistro = formatter.parse(fechaTempo);
+
+			boolean bandera = true;
+
+			if (isRegistro) { // Create
+				List<Horario> listaDia = horarioService.listarPorDiaAndVentanilla(fechaDiaRegistro,
+						horario.getVentanilla().getIdventanilla());
+				for (int i = 0; i < listaDia.size(); i++) {
+
+					if (hora.equals(listaDia.get(i).getHora())) {
+						errorMessage = "La ventanilla " + listaDia.get(i).getVentanilla().getNumero()
+								+ " ya no puede registrar la misma hora";
+						bandera = false;
+					}
+				}
+				if (bandera) {
+					horario.setDia(fechaDiaRegistro);
+					horario.setHora(hora);
+					successMessage = "Horario Registrado Exitosamente";
+				}
+
+			} else { // Update
+				List<Horario> listaDia = horarioService.listarPorDiaAndVentanillaAndHorario(fechaDiaRegistro,
+						horario.getVentanilla().getIdventanilla(), horario.getIdhorarios());
+				for (int i = 0; i < listaDia.size(); i++) {
+
+					if (hora.equals(listaDia.get(i).getHora())) {
+						errorMessage = "La ventanilla " + listaDia.get(i).getVentanilla().getNumero()
+								+ " ya no puede registrar la misma hora";
+						bandera = false;
+					}
+				}
+				if (bandera) {
+					horario.setDia(fechaDiaRegistro);
+					horario.setHora(horario.getHora());
+					horario.setRepeticiones(horario.getRepeticiones());
+					horario.setUsuario(horario.getUsuario());
+					horario.setVentanilla(horario.getVentanilla());
+					successMessage = "Horario Modificado Exitosamente";
+				}
+
 			}
+
+			if (bandera) {
+
+				Horario horarioTempo = new Horario();
+				horarioTempo.setDia(horario.getDia());
+				horarioTempo.setHora(horario.getHora());
+				horarioTempo.setRepeticiones(1);
+				horarioTempo.setUsuario(horario.getUsuario());
+				horarioTempo.setVentanilla(horario.getVentanilla());
+
+				if(!isRegistro){
+					horarioTempo.setIdhorarios(horario.getIdhorarios());
+				}
+				respuesta = horarioService.guardar(horarioTempo);
+				if (!respuesta) {
+					errorMessage = "Registro Fallido por favor intente de nuevo";
+				}else{
+					registrosExitosos = registrosExitosos + 1;
+				}
+
+				if(!isRegistro){
+					break;
+				}
+			}
+
+
+			SimpleDateFormat formatterTempo = new SimpleDateFormat("yyyy-MM-dd");
+			Date fechaCambio = formatter.parse(fechaTempo);
+		
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(fechaCambio);            
+			calendar.add(Calendar.DAY_OF_YEAR, 7);
+			Date dateOneWeek = calendar.getTime();
+			fechaTempo = formatterTempo.format(dateOneWeek);
+
 		}
 
-	
-		if (respuesta) {
-			redirectAttributes.addFlashAttribute("msg_success", successMessage);
-			return "redirect:/horarios/listar";
-		}else {
-			redirectAttributes.addFlashAttribute("msg_error", errorMessage);
-			if (horario.getIdhorarios() == null) {
-				return "redirect:/horarios/crear";
-			}else{
-				return "redirect:/horarios/editar/"+ horario.getIdhorarios();
+
+		if (registrosExitosos > 0) {
+			if (isRegistro){
+				redirectAttributes.addFlashAttribute("msg_success", successMessage + ", " + registrosExitosos +" Registros Exitosos");
 			}
-		
+			
+			return "redirect:/horarios/listar";
+		} else {
+			redirectAttributes.addFlashAttribute("msg_error", errorMessage);
+			if (isRegistro) {
+				return "redirect:/horarios/crear";
+			} else {
+				return "redirect:/horarios/editar/" + horario.getIdhorarios();
+			}
+
 		}
 	}
 
-    @GetMapping("/eliminar/{idhorarios}")
+	@GetMapping("/eliminar/{idhorarios}")
 	public String eliminarHorario(@PathVariable long idhorarios, RedirectAttributes redirectAttributes) {
 		boolean respuesta = horarioService.eliminar(idhorarios);
 		if (respuesta) {
 			redirectAttributes.addFlashAttribute("msg_success", "Registro Eliminado");
-			
-		}else {
+
+		} else {
 			redirectAttributes.addFlashAttribute("msg_error", "Eliminacion Fallida");
-			
+
 		}
 		return "redirect:/horarios/listar";
 	}
-
 
 }
